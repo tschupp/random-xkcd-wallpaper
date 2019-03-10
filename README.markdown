00bbb06c9118da756a707fb58f559238a1d06c29
Thomas Schupp <thomas.schupp@zalando.de>
2019-03-10 16:54:36 +0100

    initial changes to get the download work

--- README.markdown
+++ README.markdown
@@ -1,21 +1,17 @@
 What this script does
 ---------------------
-Downloads a random xkcd comic from xkcd.com and sets it as your desktop background. Only works with GNOME for the moment. 
+Downloads a random xkcd comic, adjusts it with ImageMagick and sets it as your desktop background on your mac. 
 
 How to use it
 -------------
-Get `xkcd_wallpaper.sh` either by cloning the github project or downloading the file. Run it from the terminal, with a keyboard shortcut or with a custom application launcher. You can run the script with `sh xkcd_wallpaper.sh` or `./xkcd_wallpaper.sh`. The file `current_xkcd_wallpaper.(png | jpg)` gets saved in your home directory in the hidden folder `.xkcd_wallpaper`. 
+Get `xkcd_wallpaper.sh` either by cloning the github project or downloading the file. 
+Install ImageMagick (`ie brew install imagemagick`)
 
-Additional remarks
------------------
-* I have only tested this script on ubuntu 10.04 and 10.10.
+Run it from the terminal, with a keyboard shortcut or with a custom application launcher. The file `current_xkcd_wallpaper.(png | jpg)` gets saved in `$HOME/.xkcd_wallpaper`.
+
+Gnome remarks
+-------------
+Adopted from https://github.com/Basphil/random-xkcd-wallpaper, I have not tested in on gnome at all. Original notes:
 * To see which file is currently set as wallpaper. Run `gconf-editor` and have a look at the value `/desktop/gnome/background/picture_filename`. This file will not exist anymore though, as it gets moved to `current_xkcd_wallpaper.(png | jpg)`
 * Running the script with a keyboard shortcut set in the ubuntu Keyboard Shortcuts menu does not work for the moment. It does work with compiz keyboard shortcuts though. 
-* Running it with cron did not work either when I tested it. I will look into this issue as well. Please let me know if it works on your computer.
-* If you have any feedback (negative or positive) drop me an e-mail at basil.philipp@gmail.com
 
-Future features
----------------
-* Display titles.
-* Make it possible to choose the location where the picture file gets saved.
-* Update background when new comic is published.
--- xkcd_wallpaper.sh
+++ xkcd_wallpaper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#    random xkcd wallpaper: gets a random xkcd comic from xkcd.com and sets it as the desktop background.
+#    random xkcd wallpaper: Downloads a random xkcd comic, adjusts it with ImageMagick and sets it as your desktop background.
 #    Copyright (C) 2011  Basil Philipp, basil.philipp@gmail.com
 
 #    This program is free software: you can redistribute it and/or modify
@@ -16,26 +16,30 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-
 #Creates working directory. Picture will be saved in this directory.
 directory="$HOME/.xkcd_wallpaper"
-
-if [ ! -d "$directory" ]
-then
-	mkdir $directory
-fi
-
+if [ ! -d "$directory" ]; then mkdir $directory; fi
 cd $directory
+
+# oldschool helper
+log() {
+    echo ">> $1" >&2
+}
 
 # /random/comic redirects to random comic.
 #Searches the line in the html that points to the url where the actual comic is placed.
 #The image urls are of the form: http://imgs.xkcd.com/comics/.'name of comic'.(png | jpg)
-url=$(curl -sL http://dynamic.xkcd.com/random/comic/ | grep -om1 'http://imgs.xkcd.com/comics/[^.]*\.[a-z]*')
+getImageUrl() {                                                                                     # get the last img:
+    curl -sL http://dynamic.xkcd.com/random/comic/ | grep -om1 'imgs.xkcd.com/comics/[^.]*\.[a-z]*' | tail -1 | awk '{print $NF}'
+}
+
+
+url="http://$(getImageUrl)"
+log "url: $url"
 
 #Assuming picture format is .png. Gets the name of the image file by only matching what comes after the last forward slash.
 name_pic=$(echo $url | grep -o [^/]*\.png)
 is_png=1
-
 
 #Sets name_pic in the case of the picture being in .jpg format.
 if [ -z "$name_pic" ]
@@ -44,19 +48,18 @@ then
     is_png=0   
 fi     
 
-
+log "download $url --> $name_pic"
 #Downloads the image and saves it under its appropriate name.
-wget --output-document="$name_pic"  "$url"
+curl -so "$name_pic" "$url"
 
-#Sets the desktop background
-gconftool-2 --set --type=string /desktop/gnome/background/picture_filename $(pwd)/"$name_pic"
-
-#For some reason, if the image is moved to fast (e.g without a wait) the background does not get set.
-sleep 1
+setDesktopGnome() {
+    gconftool-2 --set --type=string /desktop/gnome/background/picture_filename $(pwd)/"$name_pic"
+    #For some reason, if the image is moved to fast (e.g without a wait) the background does not get set.
+    sleep 1
+}
  
 #The current wallpaper can always be found under "current_xkcd_wallpaper.png" or "current_xkcd_wallpaper.jpg". Also prevents cluttering the directory with images. 
 #If you want to keep all the pictures you downloaded, uncomment the following lines.
-
 if [ $is_png = 1 ] ; then
     mv $(pwd)/"$name_pic" $(pwd)/current_xkcd_wallpaper.png
 else
