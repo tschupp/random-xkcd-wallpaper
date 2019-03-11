e62b361e9df256c3def2ca5ddbcf375ad30ef86c
Thomas Schupp <thomas.schupp@zalando.de>
2019-03-12 00:22:27 +0100

    image creation and setting the wallpaper works

--- xkcd_wallpaper.sh
+++ xkcd_wallpaper.sh
@@ -16,7 +16,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-#Creates working directory. Picture will be saved in this directory.
+# Creates working directory. Picture will be saved in this directory.
 directory="$HOME/.xkcd_wallpaper"
 if [ ! -d "$directory" ]; then mkdir $directory; fi
 cd $directory
@@ -27,41 +27,61 @@ log() {
 }
 
 # /random/comic redirects to random comic.
-#Searches the line in the html that points to the url where the actual comic is placed.
-#The image urls are of the form: http://imgs.xkcd.com/comics/.'name of comic'.(png | jpg)
-getImageUrl() {                                                                                     # get the last img:
+# Searches the line in the html that points to the url where the actual comic is placed.            # there are sometimes
+# The image urls are of the form: http://imgs.xkcd.com/comics/.'name of comic'.(png | jpg).         # several images,
+getImageUrl() {                                                                                     # lets get the last one:
     curl -sL http://dynamic.xkcd.com/random/comic/ | grep -om1 'imgs.xkcd.com/comics/[^.]*\.[a-z]*' | tail -1 | awk '{print $NF}'
+}
+
+getScreenDimension() {
+    system_profiler SPDisplaysDataType | grep -m1 Resolution | sed -e 's/[^0-9]*\([0-9]*\) *x *\([0-9]*\).*/\1x\2/'
+}
+getX() {
+    echo $1 | awk -Fx '{print $1}'
+}
+getY() {
+    echo $1 | awk -Fx '{print $2}'
 }
 
 
 url="http://$(getImageUrl)"
-log "url: $url"
+name_pic=$(echo $url | grep -o [^/]*$)
 
-#Assuming picture format is .png. Gets the name of the image file by only matching what comes after the last forward slash.
-name_pic=$(echo $url | grep -o [^/]*\.png)
-is_png=1
-
-#Sets name_pic in the case of the picture being in .jpg format.
-if [ -z "$name_pic" ]
-then
-    name_pic=$(echo $url | grep -o [^/]*\.jpg)
-    is_png=0   
-fi     
-
-log "download $url --> $name_pic"
-#Downloads the image and saves it under its appropriate name.
+log "Download: $url --> $name_pic"
 curl -so "$name_pic" "$url"
 
+imgDimension=`convert $name_pic -format "%wx%h" info:`
+screenDimension=$(getScreenDimension)
+log "Dimensions: img=$imgDimension screen=$screenDimension"
+
+
+if (( `getX $imgDimension` > `getY $imgDimension` )); then
+    log "Format: landscape"
+    gravity=North
+else
+    log "Format: portrait"
+    gravity=East
+fi
+
+wallpaper_name="xkcd-wallpaper-$name_pic.png";
+convert $name_pic -set colorspace Gray -negate -resize 1000x1000 -gravity $gravity -background black -extent $screenDimension $wallpaper_name
+echo "Wallpaper generated --> $wallpaper_name"
+
 setDesktopGnome() {
-    gconftool-2 --set --type=string /desktop/gnome/background/picture_filename $(pwd)/"$name_pic"
+    gconftool-2 --set --type=string /desktop/gnome/background/picture_filename $(pwd)/xkcd-wallpaper.png
     #For some reason, if the image is moved to fast (e.g without a wait) the background does not get set.
     sleep 1
 }
  
-#The current wallpaper can always be found under "current_xkcd_wallpaper.png" or "current_xkcd_wallpaper.jpg". Also prevents cluttering the directory with images. 
-#If you want to keep all the pictures you downloaded, uncomment the following lines.
-if [ $is_png = 1 ] ; then
-    mv $(pwd)/"$name_pic" $(pwd)/current_xkcd_wallpaper.png
-else
-    mv $(pwd)/"$name_pic" $(pwd)/current_xkcd_wallpaper.jpg
-fi 
+setDesktopMacOs() {
+    # filename needs to be different from the last time the desktop had been set. Otherwise it will not change. Therefore $wallpaper 
+    osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$(pwd)/$wallpaper_name\"" 
+}
+
+setDesktopMacOs
+
+
+
+
+
+
